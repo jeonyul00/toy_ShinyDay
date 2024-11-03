b609030a8226e7d8485d246ea91b1f64206aa31e
jeonyul <jeonyul00@gmail.com>
2024-11-04 08:50:56 +0900

    first commit

---
+++ ShinyDay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShinyDay
+//
+//  Created by 전율 on 11/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
